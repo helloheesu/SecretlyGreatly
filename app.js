b7ba9f2fbd2bb2487ab4fbf1566a88686b15ef0c
heesu <heesu@nhnnext.org>
2014-11-05 15:04:49 +0900

    apply migration of version from 3 to 4

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(app.router);
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded());
+// app.use(express.cookieParser());
+// app.use(express.bodyParser());
+var router = express.Router();
+// app.use(app.router);
 
-app.get('/', function (request, response) { });
-app.get('/login', function (request, response) { });
-app.post('/login', function (request, response) { });
+router.get('/', function (request, response) { console.log('hi'); });
+router.get('/login', function (request, response) { });
+router.post('/login', function (request, response) { });
+
+app.use('/', router);
 
 http.createServer(app).listen(52273, function () {
 	console.log('Server running');
