91006c97b8e00a47e21dfa5fea348c2777e2b3a2
HWangJJUng <marvel94@nhnnext.org>
2014-11-13 04:23:27 +0900

    jjung
    (cherry picked from commit d0090e826bd89e99ddfd0e0f259376932f1b0342)
    
    Conflicts:
    	src/less/variables.less

---
+++ routes/insert.js
@@ -0,0 +1,31 @@
+/**
+ * Created by jjungmac on 2014. 11. 13..
+ */
+
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+    user: 'root',
+    password: 'tomntoms',
+    'database': 'movies'
+});
+
+
+router.route('/')
+    .post(function (req,res) {
+        var body = req.body;
+        client.query('INSERT INTO moviedata (movie_title,movie_img_url) VALUES (? , ?)',
+            [body.title, body.img_url], function () {
+                res.redirect('/recommend');
+            });
+    })
+    .get(function (req,res) {
+        res.render('insert');
+    });
+
+
+
+module.exports = router;
---
+++ routes/recommend.js
@@ -0,0 +1,49 @@
+/**
+ * Created by jjungmac on 2014. 11. 12..
+ */
+var express = require('express');
+var mysql = require('mysql');
+var fs = require('fs');
+var ejs = require('ejs');
+var http = require('http');
+
+
+// $ mysql -u root -pdb1004 test
+var client = mysql.createConnection({
+    user: 'root',
+    password: 'tomntoms',
+    'database': 'movies'
+});
+var router = express.Router();
+
+
+router.get('/delete:id', function (req,res) {
+    client.query('DELETE FROM products WHERE id=?', [req.param('id')], function () {
+        res.redirect('/');
+    })
+});
+
+
+router.get('/edit/:id', function (req,res) {
+    res.render('insert');
+});
+router.post('/edit/:id', function (req,res) {});
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    console.log("in the routes");
+        client.query('SELECT * FROM moviedata WHERE movie_id between 0 AND 10;', function (error, result, fields) {
+            console.log('loadMovie called');
+            if (error) {
+                console.log('쿼리 문장에 오류가 있습니다.');
+            } else {
+                console.log(result.length);
+                res.render('recommendation', {
+                    data: result
+            });
+        }
+    });
+});
+
+module.exports = router;
