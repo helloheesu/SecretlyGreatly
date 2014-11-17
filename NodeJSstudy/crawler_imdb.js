db69d1edde2dc12a513a3946b034e0395c11356e
heesu <heesu@nhnnext.org>
2014-11-18 01:32:55 +0900

    \!안돌아가는코드\! DOM parser가 안된단걸 알고 중단. JSDOM 알아볼예정.
    왓챠는 뎁스를 더들어가기 복잡할것같아서 IMDB로 돌림

---
+++ NodeJSstudy/crawler_imdb.js
@@ -0,0 +1,44 @@
+var http = require('http');
+
+
+var options = {
+  hostname: 'www.imdb.com',
+  port: 80,
+  path: '/chart/top',
+  method: 'GET'
+};
+
+var req = http.request(options, function(res) {
+  res.on('data', function(data) {
+  	var fs = require('fs');
+  	fs.writeFile('top250.html', data, 'utf8', function (error) {
+  		if (error) {console.log('fsw error : '+error);}
+  		console.log('writeFile');
+  	});
+  	fs.readFile('top250.html', function (error, data) {
+  		if (error) {console.log('fsr error : '+error);}
+  		console.log('read top250.html');
+  		// ref.: http://help.dottoro.com/ljssopjn.php
+  		if (!window.DOMParser) {console.log("no parser");}
+  		var parser = new DOMParser();
+  		var domDoc = parser.parseFromString(data, 'text/html');
+  		
+  		var list = document.getElementsByClassName('posterColumn');
+  		var linkArray = [];
+  		for(i=0;i<list.length;i++) {
+  			linkArray.push(list[i].firstElementChild.href);
+  		}
+  		// 여기까지 생각하고 돌아가는지 테스트 해보려했음. 그 이후로 href마다 다시 GET요청 구현.
+  		fs.appendFile('movieLink.html', linkArray[i], 'utf8');
+  	});
+  });
+});
+req.end();
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+
+
+
