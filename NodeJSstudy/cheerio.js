98babb8259fee3e4f872d6ca654d57906074e55b
heesu <heesu@nhnnext.org>
2014-11-18 04:48:28 +0900

    path must include '/' at the end

--- NodeJSstudy/cheerio.js
+++ NodeJSstudy/cheerio.js
@@ -5,7 +5,7 @@ console.log('read nooo_top250.html');
 var cheerio = require('cheerio'), $ = cheerio.load(data);
 $('.posterColumn').each(function (i, val) {
 	var result = val.children[0].attribs.href;
-	result = result.split('/?ref_=')[0];
+	result = result.split('?ref_=')[0];
 	fs.appendFileSync('cheerio_movieLink.txt', '    '+i+' '+result+'\n', 'utf8');
 	console.log(result);
 });
