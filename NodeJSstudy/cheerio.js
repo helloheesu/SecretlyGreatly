e16289dcd285ee437a171374ee58fc0eb54e013b
heesu <heesu@nhnnext.org>
2014-11-18 03:26:11 +0900

    href 뽑아내기 성공
    똑같이 EMFILE 에러난데다, jsdom과 달리 어차피 파일에 한번 저장해야해서 쓰고/읽고 따로 처리.

---
+++ NodeJSstudy/cheerio.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+var data = fs.readFileSync('nooo_top250.html', 'utf8');
+console.log('read nooo_top250.html');
+var cheerio = require('cheerio'), $ = cheerio.load(data);
+$('.posterColumn').each(function (i, val) {
+	var result = val.children[0].attribs.href;
+	result = result.split('/?ref_=')[0];
+	fs.appendFileSync('cheerio_movieLink.txt', '    '+i+' '+result+'\n', 'utf8');
+	console.log(result);
+});
+console.log('done!');
