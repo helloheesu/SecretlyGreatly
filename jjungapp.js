cf52ccdf257bedc004631a9743d9507a916ce128
HWangJJUng <marvel94@nhnnext.org>
2014-12-03 03:54:17 +0900

    delete less-middleware

--- jjungapp.js
+++ jjungapp.js
@@ -30,7 +30,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'src')));
 app.use(express.static(path.join(__dirname, 'src')));
 
 app.use('/', main);
