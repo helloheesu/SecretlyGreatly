5b118ba096374c079fcd7379a9d5033d09d5342e
yunseo <yunseo@nhnnext.org>
2014-12-03 06:33:40 +0900

    commit again. we dont know why files were gone away

---
+++ routes/evaluate.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+
+router.get('/', function (req, res) {
+    res.render('more_evaluation');
+});
+
+module.exports = router;
