d84e6a9a0700a47ea973165474b6237de3a98681
yunseo <yunseo@nhnnext.org>
2014-12-12 15:23:14 +0900

    디스플레이가 바로없어지는 문제를 해결

--- src/js/show_more.js
+++ src/js/show_more.js
@@ -1,92 +1,51 @@
- console.log('good-bye');
- 
-//seeMore 발생시키는 코드?
+//sublime 에서 jshint 플러그인을 적용해서 사용해보기.
 window.addEventListener("load", function(){
   seeMore();
-
-  getData("data/movies.json"); 
- changeData();
- //makeCardElement(data);
-   
+  console.log('good-bye');
+  getData("data/movies.json");
+  changeData();
 });
 
-
-var data;
-
-
 //더보기 
-function seeMore(){ 
+function seeMore(){
   //var showMore=document.querySelector(".see_all");
     var showMore = document.getElementsByClassName("see_all");
 
-  showMore.addEventListener('click', function(e){ 
+  showMore.addEventListener('click', function(e){
     //loading();
     
     getData("js/movies.js");
   },false);
 }
 
-   //카드 템플릿의 요소들 담기
-// var sTemplate = "<div class='poster_container'>"
-// +  "<img src='<%=ImageLink%>'>"
-// +  "<div class='top_gradation'><%=top_gradation%></div>"
-// +  "<div class='hover_container'><%=hover_container%></div>"
-// +  "<div class='black_bg'><%=black_bg%></div>"
-// +  "<div class='hover_contents'><%=hover_contents%></div>"
-// +  "<div class='btn log_container'><%=btn log_container%></div>"
-// +  "<span class='text'><%=내역보기%></div>"
-// +  "<div class='btn evaluate_container'><%=btn evaluate_container%></div>"
-// +  "<span class='text'><%=평가하기%></div>"
-
-// +"</div>" 
-
-
-
-
-
-
-var sTemplate = "<div class='poster_container'>"
-+  "<img src='<%=ImageLink%>'>"
-+  "<div class='top_gradation'></div>"
-+  "<div class='hover_container'></div>"
-+  "<div class='black_bg'></div>"
-+  "<div class='hover_contents'></div>"
-+  "<div class='btn log_container'></div>"
-+  "<span class='text'></div>"
-+  "<div class='btn evaluate_container'></div>"
-+  "<span class='text'></div>"
-
-+"</div>"     
-
-
 function makeCardElement(data){
     var done = "";
  
     for(var i = 0; i < data.length; i++){
-        var maked = sTemplate.replace("<%=link%>", data[i].PageLink)
+        var maked = sTemplate.replace("<%=link%>", data[i].PageLink);
         // .replace("<%=ImageLink%>", data[i].ImageLink)
         // .replace("<%=top_gradation%>",data[i].top_gradation)
         // .replace("<%=hover_container%>",data[i].hover_container)
         // .replace("<%=black_bg%>",data[i].black_bg)
         // .replace("<%=hover_contents%>",data[i].hover_contents)
-        done += maked; 
+        done += maked;
       }
     return done;
 }
 
 //모두보기를 누르면 카드 더보기
-function changeData() { 
+function changeData(){
   var target = document.getElementsByClassName("see_all");
 
   target.addEventListener('click', function(ev){
-  //loading();
-  console.log('hi');
-  getData(e.target.className);
-  console.log('hello');
- },false);
+  ev.preventDefault();
+  //console.log('hi');
+  getData(ev.target.className);
+  //console.log('hello');
+  },false);
+}
 
-
-function getData(dataset) { 
+function getData(dataset){
   var request = new XMLHttpRequest();
   var url = "data/movies.json";  //디비가 저장될 파일 명이 필요함. 
   request.open("GET" , url , true);
@@ -101,12 +60,5 @@ function getData(dataset) {
      var tobechanged = document.querySelector(".poster_container");
      tobechanged.innerHTML = makeCardElement(result);
     }
-  }
+  };
 }
-
-
-
-
-
-
-
