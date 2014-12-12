ed3b5f7c9df1ea5ee1945df2f9555e9aef231de5
yunseo <yunseo@nhnnext.org>
2014-12-12 15:24:55 +0900

    디스플문제를 해결

--- src/js/show_profile.js
+++ src/js/show_profile.js
@@ -1,36 +1,40 @@
 
 //장르 버튼 클릭하면 리스트 열고 닫히기
-function genreListOpenClose(){
-  var list = document.getElementById("Layer1");
-  var button = document.getElementById("profile");
-  var body = document.body;
-  var target = document.getElementById("Layer1");
+function profileListOpenClose(){
+  //element : el , array : a , object(hash table) : ht, number : n , string : s
+  var elList = document.querySelector("#Layer1");
+  var elButton = document.querySelector(".profile");
+  var elBody = document.body;
+  var elTarget = document.querySelector("#Layer1");
 
   //장르 버튼 클릭하면 리스트 오픈(touchstart)
-  button.addEventListener("click", function(e){
-    if(list.style.display ==="none"){
-      list.style.display = "block";
-    }
-    else {
-      list.style.display = "none";
-    }   
-    e.stopPropagation();
-  } , false);
+
+  //아래 eventListener가 두개 이상일 수 있음. 그러면 문제가 generListOpenClose메서드가 게속 증가할수 있음. 비대해짐.
+  //따라서 listener만을 별도로 분리.
+    elButton.addEventListener("click", function(e) {
+      buttonHandler(e,elList);
+    }, false);
 
   //다른 부분을 클릭하면 리스트 닫기
-  body.addEventListener("click", function(e){
-  if(e.target.id != "Layer1" && list.style.display === "block")
-    list.style.display = "none";
+  elBody.addEventListener("click", function(e){
+    if(e.elTarget.id != "Layer1" && elList.style.display === "block") {
+     elList.style.display = "none";
+    }
   }, false);
+}
+
+function buttonHandler(e,elList) {
+      e.preventDefault();
+      if(elList.style.display ==="none") elList.style.display = "block";
+      else elList.style.display = "none";
+      e.stopPropagation();
 }
 
 //델리게이트 
 
 window.addEventListener("load", function(){
-  genreListOpenClose();
+  //동사+명사 (무엇을하다) > toggleGenreList();
+  profileListOpenClose();
 });
-
-
-var data;
 
 
