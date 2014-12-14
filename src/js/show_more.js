f045b615a669597ae1c8101bb9ed3239d34f9ca5
yunseo <yunseo@nhnnext.org>
2014-12-14 19:37:22 +0900

    주석으로 설명달음

--- src/js/show_more.js
+++ src/js/show_more.js
@@ -1,18 +1,24 @@
 //sublime 에서 jshint 플러그인을 적용해서 사용해보기.
 window.addEventListener("load", function(){
-  seeMore();
-  console.log('good-bye');
-  getData("data/movies.json");
+  registerEvent();
+  // seeMore(); 
+  //화면이 로딩이 되면 마우스로 모두보기를 누르면, 서버에 카드이미지를요청받아서
+  //메인화면 카드 아래에 출력한다. 
+  console.log('good-bye1');
+  // getData("data/movies.json");
   changeData();
 });
+
+function registerEvent(){
+
+}
 
 //더보기 
 function seeMore(){
   //var showMore=document.querySelector(".see_all");
     var showMore = document.getElementsByClassName("see_all");
-
+console.log('good-bye!2');
   showMore.addEventListener('click', function(e){
-    //loading();
     
     getData("js/movies.js");
   },false);
@@ -22,12 +28,7 @@ function makeCardElement(data){
     var done = "";
  
     for(var i = 0; i < data.length; i++){
-        var maked = sTemplate.replace("<%=link%>", data[i].PageLink);
-        // .replace("<%=ImageLink%>", data[i].ImageLink)
-        // .replace("<%=top_gradation%>",data[i].top_gradation)
-        // .replace("<%=hover_container%>",data[i].hover_container)
-        // .replace("<%=black_bg%>",data[i].black_bg)
-        // .replace("<%=hover_contents%>",data[i].hover_contents)
+        var maked = sTemplate.replace("<%=ImageLink%>", data[i].PageLink);
         done += maked;
       }
     return done;
@@ -51,12 +52,15 @@ function getData(dataset){
   request.open("GET" , url , true);
   request.send(null);
   
-  request.onreadystatechange = function() {
+  request.onreadystatechange = function() { //콜백함수 이벤트큐에 보관이 된다. 
+  //불리지 않아도 실행이 된다. 실행될 조건이 성립이되면 불리지 않아도 실행된다. 조건은 아래if문
+  //readystate는 서버에서 도착했다고 알려주는 번호. 
 
     if(request.readyState === 4 && request.status === 200) {
       result = request.responseText;
       result = JSON.parse(result);
-     
+     //문자열 형태로 서버-클라이언트 사이에 통신한다. (타입없음)
+     //문자열 파싱을 json형식으로 하는 과정을 거침. 
      var tobechanged = document.querySelector(".poster_container");
      tobechanged.innerHTML = makeCardElement(result);
     }
