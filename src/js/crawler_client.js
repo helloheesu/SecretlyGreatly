fa1f2df0b8b6c734df3d294aa12149f71669e94e
heesu <heesu@nhnnext.org>
2014-12-27 04:11:42 +0900

    do it with for loop

--- src/js/crawler_client.js
+++ src/js/crawler_client.js
@@ -1,9 +1,14 @@
 var fs = require('fs');
 var imdb_crawler = require('./crawler.js');
-var c = new imdb_crawler(1);
-c.requestMovieInfo.call(c, function () {
-	console.log('callback called');
-	fs.appendFile('result.html', c.data, 'utf8', function(err) {
-		if(err) throw err;
-	});
-});
+
+for (var i = 0; i < 10; i++) {
+	(function (i) {
+		var c = new imdb_crawler(i);
+		c.requestMovieInfo.call(c, function () {
+			console.log('callback called');
+			fs.appendFile('movie'+i+'.html', c.data, 'utf8', function(err) {
+				if(err) throw err;
+			});
+		});		
+	})(i);
+}
