7e7b94c3799fbc35c27a4ee59c7a3deeb15f3af1
heesu <heesu@nhnnext.org>
2014-12-28 16:07:25 +0900

    insert into db

--- src/js/crawler_client.js
+++ src/js/crawler_client.js
@@ -1,14 +1,51 @@
 var fs = require('fs');
+var mysql = require('mysql');
+
 var imdb_crawler = require('./crawler.js');
 
-for (var i = 0; i < 10; i++) {
-	(function (i) {
-		var c = new imdb_crawler(i);
-		c.requestMovieInfo.call(c, function () {
-			console.log('callback called');
-			fs.appendFile('movie'+i+'.html', c.data, 'utf8', function(err) {
-				if(err) throw err;
-			});
-		});		
-	})(i);
-}
+var sqlConn = mysql.createConnection({
+	user: 'guest_demo',
+	database: 'movies'
+});
+
+
+// for (var i = 0; i < 10; i++) {
+// 	(function (i) {
+// 		var c = new imdb_crawler(i);
+// 		c.requestMovieInfo.call(c, function () {
+// 			console.log('callback called');
+// 			fs.appendFile('movie'+i+'.html', c.data, 'utf8', function(err) {
+// 				if(err) throw err;
+// 			});
+// 		});		
+// 	})(i);
+// }
+
+
+/*
+create table movie(
+	mID int primary key auto_increment,
+	title varchar(255) not null,
+	year int,
+	poster_url varchar(2000)
+);
+*/
+var movieID = 23;
+var c = new imdb_crawler(movieID);
+c.requestMovieInfo.call(c, function () {
+	c.parseData.call(c);
+
+	// console.log(c.movieData);
+	// var statement = 'INSERT INTO moviedata (mID, title, year, poster_url)'
+	// 	+' VALUES('+sqlConn.escape(parseInt(c.movieID))+','+sqlConn.escape(c.movieData.title)+','+sqlConn.escape(parseInt(c.movieData.year))+','+sqlConn.escape(c.movieData.poster)+');';
+	// console.log('state: \n'+statement);
+
+	var statement = 'INSERT INTO moviedata (mID, title, year, poster_url) VALUES(?, ?, ?, ?);';
+	sqlConn.query(statement, [parseInt(c.movieID), c.movieData.title, parseInt(c.movieData.year), c.movieData.poster], function (err, result) {
+		if(err) throw err;
+		else {
+			// if (result) console.log('result:'+result);
+			console.log(c.movieID+' inserted!');
+		}
+	});
+});
