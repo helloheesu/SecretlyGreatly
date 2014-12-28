f7c6b30b9769aaa00ffa2a265b28e45d38866163
heesu <heesu@nhnnext.org>
2014-12-28 17:43:04 +0900

    add user privileges sql

--- src/DB/user.sql
+++ src/DB/user.sql
@@ -1,9 +1,13 @@
+USE movies;
+
 CREATE TABLE user (
 	UID INT PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(50) NOT NULL UNIQUE,
 	password VARCHAR(50) NOT NULL,
 	username VARCHAR(50) 
 );
+GRANT select, insert
+	ON movies.user TO 'guest_demo'@'%';
 
 CREATE TABLE movie(
 	mID INT PRIMARY KEY AUTO_INCREMENT,
@@ -11,3 +15,7 @@ CREATE TABLE movie(
 	year INT,
 	poster_url VARCHAR(2000)
 );
+GRANT select, insert
+	ON movies.user TO 'guest_demo'@'%';
+# 사실 guest_demo 로 하면 안 됨. 다른 유저를 생성해야ㅜㅜ
+# 유저 이름도 guest_demo 말고 다른 걸로 바꿔야ㅜㅜ
