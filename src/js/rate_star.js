af8afdcbd4edc311396d643d3c1d271b82051b41
yunseo <yunseo@nhnnext.org>
2014-12-28 20:09:51 +0900

    별점을 주고 다시 클릭하면 페이지 변경없이 점수를 줄 수있는 코드 구현

--- src/js/rate_star.js
+++ src/js/rate_star.js
@@ -8,24 +8,64 @@ function mouse_enter() {
 
     $(this).addClass("hover");
 
-    var stars, target_value;
+    var stars, target_value, isclicked;
     stars = $("span.star.half", this);
+    target_value = 0;
+    isclicked = false;
 
-    function _star_over_animation(v , b) {
-        $.each(stars, function () {
-            if ($(this).attr('data-value') <= v) {
-                if( b === true ) {
+    function _star_over_animation(v, b) {
+        if (b === true) { 
+            $.each(stars, function () {
+                if($(this).attr('data-value') <= v) {
                     $(this).addClass("over");
-                } else{
-                    $(this).removeClass("over");
+                } else {
+                    $(this).removeClass("over");   
                 }
-            }
-        });
+            });
+        } else {
+            $.each(stars, function () {
+                $(this).removeClass("over");   
+            }); 
+        }
+            
+            // if ($(this).attr('data-value') <= v) {
+            //     if( b === true ) {
+            //         $(this).addClass("over");
+            //     } else{
+            //         $(this).removeClass("over");
+            //     }
+            // } else {
+            //     if( b === false ) {
+            //         $(this).addClass("over");
+            //     } else{
+            //         $(this).removeClass("over");
+            //     }
+            // }
     }
 
     function _showDataLoadingPage() {
-       console.log(target_value);
+        console.log(target_value);       //콘솔 창에 클릭한 별점을 보여줌
+    
+        var x = document.getElementById("Director_score").value;
+      
+        // console.log(document.getElementById("Director_score").value);
     }
+
+
+    function _changeStarValue() {
+       console.log(target_value);
+
+     // input type="hidden" name="Director_score" value="" >
+     //ejs파일에서 이름에 맞는 value 값을 해당하는 값으로 동적으로 변환해줌
+
+
+        
+        _saveStarValuetoDb();//디비에 저장하도록 해주는 함수 호출 
+    }
+
+    // function _saveStarValuetoDb() {
+  
+    // }    
 
     /*
      별점 클릭 이벤트 연결.
@@ -35,6 +75,10 @@ function mouse_enter() {
 
     $(stars).click(function () {
         _showDataLoadingPage();
+        isclicked = true;
+        $("span.star.half",this).off();
+        // target value가 리스트에 저장되이써야해.
+        // 그래서 호버 이벤트 할떄마다 저장된 타겟벨류로 이니셜라이즈를 해야해. 
     });
 
     // 별점 드래그 이벤트 연결
@@ -45,8 +89,10 @@ function mouse_enter() {
             _star_over_animation(target_value , true);
         },
         function () {
+            if(isclicked === false) {
             $(this).removeClass("hover");
-            _star_over_animation(target_value , false);
+             _star_over_animation(target_value , false);
+            }
         });
 }
 
@@ -55,4 +101,4 @@ function mouse_leave() {
     $("span.star.half",this).off();
 }
 
-$(".star_container").hover(mouse_enter,mouse_leave);
+$(".star-card").hover(mouse_enter,mouse_leave);
