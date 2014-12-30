48ed2ef5b8bcdb399d75aa500dcf7aaf6bea52db
yunseo <yunseo@nhnnext.org>
2014-12-31 01:37:23 +0900

    마스터브랜치와 합치기 전 Ajax브랜치 커밋

--- routes/evaluate_crew.js
+++ routes/evaluate_crew.js
@@ -16,16 +16,21 @@ var router = express.Router();
 
 router.get('/', function (request, response, next) {
 	console.log('hello ');
-	// client.query('SELECT movie_title FROM moviedata where movie_title="daeboo";'), function (error, db_title_result, fields) {
+	client.query('SELECT movie_title FROM moviedata where movie_title="daeboo";', function (error, db_title_result, fields) {
+		console.log(db_title_result);
+		if(error) console.log('movie_title error');
 		client.query('SELECT name, picture, role FROM crew_info where type = "director";', function (error, db_director_result, fields) {
+			if(error) console.log('director error');
 			client.query('SELECT name, picture, role FROM crew_info where type = "actor";', function (error, db_actor_result, fields) {
+				if(error) console.log('actor error');
 				client.query('SELECT name, picture, role FROM crew_info where type = "story";', function (error, db_story_result, fields) {
+					if(error) console.log('story error');
 					client.query('SELECT name, picture, role  FROM crew_info where type = "music";', function (error, db_music_result, fields) {
+						if(error) console.log('music error');
 						client.query('SELECT name, picture, role FROM crew_info where type = "visual";', function (error, db_visual_result, fields) {
-
-
-		response.render('more_evaluation', {director_result:db_director_result, actor_result:db_actor_result,story_result:db_story_result, music_result:db_music_result, visual_result:db_visual_result} );
-
+							if(error) console.log('visual error');
+							response.render('more_evaluation', {title_result:db_title_result, director_result:db_director_result, actor_result:db_actor_result,story_result:db_story_result, music_result:db_music_result, visual_result:db_visual_result} );
+						});
 					});
 				});
 			});
