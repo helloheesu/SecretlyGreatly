42f07e22870c5025af271f0dc975c194424e65ca
heesu <heesu@nhnnext.org>
2014-12-30 22:07:18 +0900

    change record erd

--- src/DB/relations.sql
+++ src/DB/relations.sql
@@ -30,7 +30,7 @@ GRANT select, insert
 	ON movies.participate TO 'guest_demo'@'%';
 INSERT INTO participate (mID, cID, tID, role, credit_order) VALUES(472160, 564215, 3, 'Johnny / Max', 15);
 
-CREATE TABLE record(
+CREATE TABLE total_record(
 	rID INT NOT NULL AUTO_INCREMENT,
 	timestamp TIMESTAMP NOT NULL,
 	PRIMARY KEY (rID, timestamp),
@@ -38,11 +38,27 @@ CREATE TABLE record(
 	mID INT NOT NULL,
 	total_score DECIMAL(2, 1),
 	comment TEXT,
+	FOREIGN KEY (uID) REFERENCES user(uID) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (mID) REFERENCES movie(mID) ON UPDATE CASCADE ON DELETE CASCADE
+);
+GRANT select, insert
+	ON movies.total_record TO 'guest_demo'@'%';
+CREATE TABLE record(
+	rID INT NOT NULL AUTO_INCREMENT,
+	timestamp TIMESTAMP NOT NULL,
+	PRIMARY KEY (rID, timestamp),
+	uID INT NOT NULL,
+	mID INT NOT NULL,
 	tID INT NOT NULL,
 	type_score DECIMAL(2,1),
 	FOREIGN KEY (uID) REFERENCES user(uID) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (mID) REFERENCES movie(mID) ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY (tID) REFERENCES type(tID) ON UPDATE CASCADE ON DELETE CASCADE
+	FOREIGN KEY (tID) REFERENCES type(tID) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (rID) REFERENCES total_record(rID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 GRANT select, insert
 	ON movies.record TO 'guest_demo'@'%';
+
+-- CREATE
+INSERT INTO total_record (uID, mID, total_score, comment) VALUES (SELECT uID FROM user WHERE username='helloheesu', 472160, 4.0, '맥어보이b');
+INSERT INTO record (rID, uID, mID, tID, type_score) VALUES (SELECT LAST_INSERT_ID(), SELECT uID FROM user WHERE username='helloheesu', 472160, 3, 5.0);
