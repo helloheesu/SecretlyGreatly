e5d60da1b0bb349bc79d739b9bdb453b3f7c2c38
heesu <heesu@nhnnext.org>
2014-12-31 01:19:30 +0900

    insert into sql crawled crew info

--- src/js/crawler_client.js
+++ src/js/crawler_client.js
@@ -68,24 +68,58 @@ var pool  = mysql.createPool({
 	database: 'movies'
 });
 
-var movieID = 268126;
+var movieID = 472160;
 var pc = new participationCrawler(movieID);
 pc.requestParticipationInfo.call(pc, function () {
 	// INSERT INTO participate (mID, cID, tID, role, credit_order) VALUES(472160, 564215, 3, 'Johnny / Max', 15);
 	pc.parseData.call(pc);
 	var pData = pc.participationData;
 	for (var type in pData) {
-		console.log(type+'========================');
-		var typeArray = pData[type];
-		for (var i = 0; i < typeArray.length; i++) {
-			var crewID = typeArray[i].cID;
-			console.log((i+1)+'th : '+crewID);
-			// var cc = new crewCrawler(crewID);
-			// cc.requestCrewInfo.call(cc, function () {
-			// 	cc.parseData.call(cc);
-			// 	console.log(cc.crewData);
-			// });
-		}
+		(function(type){
+			console.log(type+'========================');
+			var typeArray = pData[type];
+			console.log(typeArray);
+
+			// INSERT INTO crew (cID, name, profile_url) VALUES(564215, 'James McAvoy', 'http://ia.media-imdb.com/images/M/MV5BMTQzNzIxOTYzMl5BMl5BanBnXkFtZTcwNjYxNTk1Nw@@._V1_SY317_CR14,0,214,317_AL_.jpg');
+			// this.crewID / this.crewData = {name: name, profile: profileSrc};
+			var crewInsertSql = 'INSERT INTO crew (cID, name, profile_url) VALUES(?, ?, ?);';
+			// !!!!!!!! 중요 !!!!!!!! 위에 getConnection 을 똥 같이 한 것 같다.
+			pool.getConnection(function(err, sqlConn) {
+				if(err) {
+					console.log('pool connection error');
+					throw err;
+				}
+				var insertedOK = 0;
+				for (var i = 0; i < typeArray.length; i++) {
+					(function(i){
+						var crewID = typeArray[i].cID;
+						var cc = new crewCrawler(crewID);
+						cc.requestCrewInfo.call(cc, function () {
+							cc.parseData.call(cc);
+							console.log('crewID : '+cc.crewID);
+							sqlConn.query(crewInsertSql, [cc.crewID, cc.crewData.name, cc.crewData.profile], function (err, result) {
+								if(err) {
+									if(err.code == 'ER_DUP_ENTRY') {
+										console.log('crew#'+cc.crewID+' already exists');
+										return;
+									}
+									console.log('error!!! crewID : '+cc.crewID);
+									throw err;
+								} else {
+									console.log('crew#'+cc.crewID+' inserted!');
+									console.log('inserted crew: '+(++insertedOK));
+									if(insertedOK >= typeArray.length) {
+										console.info('will release sqlConn');
+										sqlConn.release();
+									}
+								}
+							});
+						});
+						// console.log(cc.crewData);
+					})(i);
+				}
+			});
+		})(type);
 	}
 	// console.log(pc.participationData);
 });
