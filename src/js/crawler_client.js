c4eccf6b8f3d20c3225e907522de4fe1b1b3e8ca
heesu <heesu@nhnnext.org>
2014-12-30 11:38:06 +0900

    remove useless line : no need immediate func

--- src/js/crawler_client.js
+++ src/js/crawler_client.js
@@ -11,51 +11,49 @@ var pool  = mysql.createPool({
 });
 
 for (var i = 1; i < 10000; i++) {
-	(function (i) {
-		var c = new movieCrawler(i);
-		c.requestMovieInfo.call(c, function () {
-			c.parseData.call(c);
-			var movieInsertSql = 'INSERT INTO movie (mID, title, year, poster_url) VALUES(?, ?, ?, ?);';
-			pool.getConnection(function(err, sqlConn) {
-				sqlConn.query(movieInsertSql, [c.movieID, c.movieData.title, c.movieData.year, c.movieData.poster], function (err, result) {
-					console.info('will release sqlConn');
-					sqlConn.release();
+	var c = new movieCrawler(i);
+	c.requestMovieInfo.call(c, function () {
+		c.parseData.call(c);
+		var movieInsertSql = 'INSERT INTO movie (mID, title, year, poster_url) VALUES(?, ?, ?, ?);';
+		pool.getConnection(function(err, sqlConn) {
+			sqlConn.query(movieInsertSql, [c.movieID, c.movieData.title, c.movieData.year, c.movieData.poster], function (err, result) {
+				console.info('will release sqlConn');
+				sqlConn.release();
+				if(err) {
+					if(err.code == 'ER_DUP_ENTRY') {
+						console.log('movie#'+c.movieID+' already exists');
+						return;
+					}
+					throw err;
+				} else {
+					// if (result) console.log('result:'+result);
+					console.log(c.movieID+' inserted!');
+				}
+			});
+		});
+		var genreInsertSql = 'INSERT INTO genre (mID, genre) VALUES(?, ?);';
+		pool.getConnection(function(err, sqlConn) {
+			var insertedOK = 0;
+			for (var i = 0; i < c.movieData.genre.length; i++) {
+				sqlConn.query(genreInsertSql, [c.movieID, c.movieData.genre[i]], function (err, result) {
 					if(err) {
 						if(err.code == 'ER_DUP_ENTRY') {
-							console.log('movie#'+c.movieID+' already exists');
+							console.log('movie#'+c.movieID+' genre#'+i+' already exists');
 							return;
 						}
 						throw err;
 					} else {
-						// if (result) console.log('result:'+result);
-						console.log(c.movieID+' inserted!');
+						console.log('movie#'+c.movieID+' genre#'+i+' inserted!');
+						console.log('inserted genre: '+(++insertedOK));
+						if(insertedOK >= c.movieData.genre.length) {
+							console.info('will release sqlConn');
+							sqlConn.release();
+						}
 					}
 				});
-			});
-			var genreInsertSql = 'INSERT INTO genre (mID, genre) VALUES(?, ?);';
-			pool.getConnection(function(err, sqlConn) {
-				var insertedOK = 0;
-				for (var i = 0; i < c.movieData.genre.length; i++) {
-					sqlConn.query(genreInsertSql, [c.movieID, c.movieData.genre[i]], function (err, result) {
-						if(err) {
-							if(err.code == 'ER_DUP_ENTRY') {
-								console.log('movie#'+c.movieID+' genre#'+i+' already exists');
-								return;
-							}
-							throw err;
-						} else {
-							console.log('movie#'+c.movieID+' genre#'+i+' inserted!');
-							console.log('inserted genre: '+(++insertedOK));
-							if(insertedOK >= c.movieData.genre.length) {
-								console.info('will release sqlConn');
-								sqlConn.release();
-							}
-						}
-					});
-				}
-			});
+			}
 		});
-	})(i);
+	});
 }
 */
 
