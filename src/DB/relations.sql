5e2882e4417249dd5ae7deae8f74829900720e38
heesu <heesu@nhnnext.org>
2014-12-30 16:48:38 +0900

    rename properties and tables

--- src/DB/relations.sql
+++ src/DB/relations.sql
@@ -1,25 +1,25 @@
-CREATE TABLE p_type(
-	typeID INT PRIMARY KEY,
-	type_name VARCHAR(255) NOT NULL
+CREATE TABLE type(
+	tID INT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL
 );
 GRANT select
-	ON movies.p_type TO 'guest_demo'@'%';
-INSERT INTO p_type VALUES(1, 'direction');
-INSERT INTO p_type VALUES(2, 'scenario');
-INSERT INTO p_type VALUES(3, 'acting');
-INSERT INTO p_type VALUES(4, 'music');
-INSERT INTO p_type VALUES(5, 'cinematography');
+	ON movies.type TO 'guest_demo'@'%';
+INSERT INTO type VALUES(1, 'direction');
+INSERT INTO type VALUES(2, 'scenario');
+INSERT INTO type VALUES(3, 'acting');
+INSERT INTO type VALUES(4, 'music');
+INSERT INTO type VALUES(5, 'cinematography');
 
-CREATE TABLE participation(
-	pID INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE participate(
 	mID INT NOT NULL,
 	cID INT NOT NULL,
-	typeID INT NOT NULL,
-	rank INT NOT NULL,
+	tID INT NOT NULL,
 	role VARCHAR(255),
+	credit_order INT NOT NULL,
 	FOREIGN KEY (mID) REFERENCES movie(mID) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (cID) REFERENCES crew(cID) ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY (typeID) REFERENCES p_type(typeID) ON UPDATE CASCADE ON DELETE CASCADE
+	FOREIGN KEY (tID) REFERENCES type(tID) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY (mID, cID, tID)
 );
 GRANT select, insert
 	ON movies.participation TO 'guest_demo'@'%';
